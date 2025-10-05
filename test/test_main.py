f0bea32a7ed6fe05a3682b32de109ae170673339
ghoshshawon <ghoshshawon491@gmail.com>
2025-10-05 20:10:39 +0530

    push

--- test/test_main.py
+++ test/test_main.py
@@ -12,6 +12,7 @@ class TestEvaluator:
     def test_high_amount(self, payload):
         evaluator = Evaluator(payload)
         results = evaluator.evaluate()
+        print("\nTRACE:", results[0]['trace'])
 
         found = False
         for rule_res in results:
@@ -26,6 +27,7 @@ class TestEvaluator:
     def test_geo_mismatch(self, payload):
         evaluator = Evaluator(payload)
         results = evaluator.evaluate()
+        print("\nTRACE:", results[0]['trace'])
 
         found = False
         for rule_res in results:
@@ -40,12 +42,20 @@ class TestEvaluator:
     def test_score_within_bounds(self, payload):
         evaluator = Evaluator(payload)
         results = evaluator.evaluate()
+        print("\nTRACE:", results[0]['trace'])
         for r in results:
             assert 0 <= r["score"] <= 100
+    # def test_missing_attributes(self):
+    #     payload = {"amount": 2000}   
+    #     evaluator = Evaluator(payload)
+    #     results = evaluator.evaluate()
+    #     print("\nTRACE (missing attrs):", results[0]['trace'])
+    #     assert any("Missing attribute" in cond.get("error", "") for cond in results[0]['trace'])
 
     def test_all(self, payload):
         evaluator = Evaluator(payload)
         results = evaluator.evaluate()
+        print("\nTRACE:", results[0]['trace'])
 
         for rule_res in results:
             assert "rule_id" in rule_res
