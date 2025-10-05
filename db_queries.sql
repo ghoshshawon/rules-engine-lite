eb0f0a00a244343c112c1d244b31110ac6fe5810
ghoshshawon <ghoshshawon491@gmail.com>
2025-10-05 20:59:20 +0530

    up

--- db_queries.sql
+++ db_queries.sql
@@ -5,7 +5,7 @@ USE rules_engine_db;
 
 CREATE TABLE rules (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    rule_id VARCHAR(50),
+    rule_id VARCHAR(50) NOT NULL UNIQUE, 
     decision VARCHAR(20),
     score_delta INT,
     reason VARCHAR(255)
@@ -13,13 +13,15 @@ CREATE TABLE rules (
 
 CREATE TABLE conditions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    rule_id VARCHAR(50),
+    rule_id INT,  -- this will reference rules.id
     condition_type ENUM('all','any'),
     op VARCHAR(10),
     left_path VARCHAR(100),
     right_value JSON,
     FOREIGN KEY (rule_id) REFERENCES rules(id)
-);
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+)
 
 CREATE TABLE defaults (
     id INT AUTO_INCREMENT PRIMARY KEY,
