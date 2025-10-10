792e57c9a7ad512b11992232187274e37cd68a70
ghoshshawon <ghoshshawon491@gmail.com>
2025-10-10 23:10:01 +0530

    update

--- main.py
+++ main.py
@@ -7,36 +7,36 @@ import os
 import argparse
 from fastapi import FastAPI
 
-# payload_path="src/input_files/"
-# rules_path="src/import_rules/files/"
-# def get_data_rules(payload_file, rules_file):
-#     data = os.path.join(payload_path, payload_file)
-#     rules = os.path.join(rules_path, rules_file)
+payload_path="src/input_files/"
+rules_path="src/import_rules/files/"
+def get_data_rules(payload_file, rules_file):
+    data = os.path.join(payload_path, payload_file)
+    rules = os.path.join(rules_path, rules_file)
     
-#     return data,rules
+    return data,rules
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def get_welcome():
-     return "welcome"
+# @app.get("/")
+# def get_welcome():
+#      return "welcome"
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     parser= argparse.ArgumentParser("Rules Evaluation")
-#     parser.add_argument(
-#          "--payload",metavar="input data file",required=True,help="Please provide the Payload to verify with rules"
-#     )
-#     parser.add_argument(
-#          "--rules",metavar="rules file",required=True,help="Please provide the Rules"
-#     )
-#     args = parser.parse_args()
-#     data, rules = get_data_rules(args.payload, args.rules)
-#     os.environ["PAYLOAD_FILE"] = data
+    parser= argparse.ArgumentParser("Rules Evaluation")
+    parser.add_argument(
+         "--payload",metavar="input data file",required=True,help="Please provide the Payload to verify with rules"
+    )
+    parser.add_argument(
+         "--rules",metavar="rules file",required=True,help="Please provide the Rules"
+    )
+    args = parser.parse_args()
+    data, rules = get_data_rules(args.payload, args.rules)
+    os.environ["PAYLOAD_FILE"] = data
     
-#     db_insert = Import_to_db(rules)
-#     db_insert.insert_rules()
-#     print("\n Running Tests from main.py...\n")
-#     pytest.main(["-v", "test/test_main.py"])
+    db_insert = Import_to_db(rules)
+    db_insert.insert_rules()
+    print("\n Running Tests from main.py...\n")
+    pytest.main(["-v", "test/test_main.py"])
  
